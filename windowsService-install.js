1cc4931ab7ffd2b350a5b6d0156f483db6ea17d3
dangowans <d.gowans@cityssm.on.ca>
2020-10-05 16:02:46 -0400

    windows service scripts

---
+++ windowsService-install.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_windows_1 = require("node-windows");
+const path = require("path");
+const svc = new node_windows_1.Service({
+    name: "Mini Shop",
+    description: "A lightweight, highly customizable storefront.",
+    script: path.join(__dirname, "bin", "www.js")
+});
+svc.on("install", () => {
+    svc.start();
+});
+svc.install();
+//# sourceMappingURL=windowsService-install.js.map
---
+++ windowsService-install.ts
@@ -0,0 +1,17 @@
+import { Service } from "node-windows";
+import * as path from "path";
+
+// Create a new service object
+const svc = new Service({
+  name: "Mini Shop",
+  description: "A lightweight, highly customizable storefront.",
+  script: path.join(__dirname, "bin", "www.js")
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on("install", () => {
+  svc.start();
+});
+
+svc.install();
