c0017807dbd55623beaa3d579376f2180fcc5bd9
dangowans <d.gowans@cityssm.on.ca>
2020-10-29 11:28:29 -0400

    update dependencies, fixes #8

--- handlers/product-view.js
+++ handlers/product-view.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
+const configFns = require("../helpers/configFns");
 exports.handler = (req, res) => {
     const productSKU = req.params.productSKU;
+    if (!configFns.getProperty("products")[productSKU]) {
+        res.status(404);
+        return res.render("product-notFound");
+    }
     return res.render("product-view", {
         productSKU
     });
--- handlers/product-view.ts
+++ handlers/product-view.ts
@@ -1,9 +1,18 @@
 import type { RequestHandler } from "express";
 
+import * as configFns from "../helpers/configFns";
+
 
 export const handler: RequestHandler = (req, res) => {
 
   const productSKU = req.params.productSKU;
+
+  if (!configFns.getProperty("products")[productSKU]) {
+
+    res.status(404);
+
+    return res.render("product-notFound");
+  }
 
   return res.render("product-view", {
     productSKU
