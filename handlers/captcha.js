1e9a12528892f3c491eec142cdc74c444fc4531a
Dan Gowans <d.gowans@cityssm.on.ca>
2022-11-03 13:11:27 -0400

    increase opacity of decoy text

--- handlers/captcha.js
+++ handlers/captcha.js
@@ -10,7 +10,7 @@ export const handler = async (request, response, next) => {
     const captcha = new CaptchaGenerator()
         .setDimension(150, 500)
         .setCaptcha({ text: captchaText, size: 60, color: "#ce1036" })
-        .setDecoy({ opacity: 0.4 })
+        .setDecoy({ opacity: 0.2 })
         .setTrace({ color: "#da1039" });
     const buffer = await captcha.generate();
     response.contentType("image/png");
