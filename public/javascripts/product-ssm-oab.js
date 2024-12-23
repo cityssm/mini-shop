2595894c970857ddc3503bc03a29aa9cb9717a84
Dan Gowans <dangowans@users.noreply.github.com>
2024-12-23 11:25:39 -0500

    disable tenant applications

--- public/javascripts/product-ssm-oab.js
+++ public/javascripts/product-ssm-oab.js
@@ -1,9 +1,34 @@
 ;
 (() => {
     const formElement = document.querySelector('#form--product');
-    const radioElements = formElement.querySelectorAll('input[name="permitType"]');
+    const applicantTypeRadioElements = formElement.querySelectorAll('input[name="applicantType"]');
+    const ownerContainerElement = formElement.querySelector('div[id^="container_"][id$="_applicantType-owner"]');
+    const tenantContainerElement = formElement.querySelector('div[id^="container_"][id$="_applicantType-tenant"]');
+    function resetApplicantType() {
+        ownerContainerElement.classList.add('is-hidden');
+        tenantContainerElement.classList.add('is-hidden');
+        formElement.querySelector('input[name="understandGuidelines"]').checked = false;
+        formElement.querySelector('button[type="submit"]').disabled = true;
+    }
+    function toggleApplicantType() {
+        const applicantType = formElement.querySelector('[name="applicantType"]:checked').value;
+        resetApplicantType();
+        if (applicantType === 'owner') {
+            ownerContainerElement.classList.remove('is-hidden');
+            formElement.querySelector('button[type="submit"]').disabled = false;
+        }
+        else if (applicantType === 'tenant') {
+            tenantContainerElement.classList.remove('is-hidden');
+        }
+    }
+    resetApplicantType();
+    for (const radioElement of applicantTypeRadioElements) {
+        radioElement.checked = false;
+        radioElement.addEventListener('change', toggleApplicantType);
+    }
+    const permitTypeRadioElements = formElement.querySelectorAll('input[name="permitType"]');
     function setUnitPrice() {
-        for (const radioElement of radioElements) {
+        for (const radioElement of permitTypeRadioElements) {
             if (radioElement.checked) {
                 ;
                 formElement.querySelector('[name="unitPrice"]').value = radioElement.dataset.unitPrice ?? '';
@@ -11,7 +36,7 @@
         }
     }
     setUnitPrice();
-    for (const radioElement of radioElements) {
+    for (const radioElement of permitTypeRadioElements) {
         radioElement.addEventListener('change', setUnitPrice);
     }
 })();
--- public/javascripts/product-ssm-oab.ts
+++ public/javascripts/product-ssm-oab.ts
@@ -3,12 +3,63 @@
     '#form--product'
   ) as HTMLFormElement
 
-  const radioElements = formElement.querySelectorAll(
+  /*
+   * Applicant Type
+   */
+
+  const applicantTypeRadioElements = formElement.querySelectorAll(
+    'input[name="applicantType"]'
+  ) as NodeListOf<HTMLInputElement>
+
+  const ownerContainerElement = formElement.querySelector(
+    'div[id^="container_"][id$="_applicantType-owner"]'
+  ) as HTMLElement
+
+  const tenantContainerElement = formElement.querySelector(
+    'div[id^="container_"][id$="_applicantType-tenant"]'
+  ) as HTMLElement
+
+  function resetApplicantType(): void {
+    ownerContainerElement.classList.add('is-hidden')
+    tenantContainerElement.classList.add('is-hidden')
+    ;(formElement.querySelector('input[name="understandGuidelines"]') as HTMLInputElement).checked = false
+    ;(formElement.querySelector('button[type="submit"]') as HTMLButtonElement).disabled = true
+  }
+
+  function toggleApplicantType(): void {
+    const applicantType = (
+      formElement.querySelector(
+        '[name="applicantType"]:checked'
+      ) as HTMLInputElement
+    ).value
+
+    resetApplicantType()
+
+    if (applicantType === 'owner') {
+      ownerContainerElement.classList.remove('is-hidden')
+      ;(formElement.querySelector('button[type="submit"]') as HTMLButtonElement).disabled = false
+    } else if (applicantType === 'tenant') {
+      tenantContainerElement.classList.remove('is-hidden')
+    }
+  }
+
+  resetApplicantType()
+
+  for (const radioElement of applicantTypeRadioElements) {
+    radioElement.checked = false
+    radioElement.addEventListener('change', toggleApplicantType)
+  }
+
+  /*
+   * Unit Price
+   */
+
+  const permitTypeRadioElements = formElement.querySelectorAll(
     'input[name="permitType"]'
   ) as NodeListOf<HTMLInputElement>
 
   function setUnitPrice(): void {
-    for (const radioElement of radioElements) {
+    for (const radioElement of permitTypeRadioElements) {
       if (radioElement.checked) {
         ;(
           formElement.querySelector('[name="unitPrice"]') as HTMLInputElement
@@ -19,7 +70,7 @@
 
   setUnitPrice()
 
-  for (const radioElement of radioElements) {
+  for (const radioElement of permitTypeRadioElements) {
     radioElement.addEventListener('change', setUnitPrice)
   }
 })()
